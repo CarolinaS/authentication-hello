98faf3196d1b97a4ffb6d0cd8fecfe2bae0767c9
CarolinaS <crcr_14@hotmail.com>
2018-08-06 13:15:14 -0500

    authentication login

---
+++ src/main/java/com/tcs/authenticationhello/AuthenticationHelloApplication.java
@@ -0,0 +1,12 @@
+package com.tcs.authenticationhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationHelloApplication.class, args);
+	}
+}
